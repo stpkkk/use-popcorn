7eff276db55667f1a199c1b59b6d0a5062dcb510
Igor Stepanov <stipyk1309@gmail.com>
2024-05-04 18:46:57 +0300

    feat: Search input focus on Enter keydown with useRef

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 interface ISearch {
 	setQuery: React.Dispatch<React.SetStateAction<string>>
 	query: string
 }
 const Search: React.FC<ISearch> = ({ setQuery, query }) => {
+	const inputEl = useRef<HTMLInputElement>(null)
+
+	useEffect(() => {
+		const callback = (e: any) => {
+			if (document.activeElement === inputEl.current) return
+
+			if (e.code === 'Enter') {
+				inputEl.current?.focus()
+				setQuery('')
+			}
+		}
+
+		document.addEventListener('keydown', callback)
+		return () => document.addEventListener('keydown', callback)
+	}, [setQuery])
+
 	return (
 		<input
 			className='search'
+			ref={inputEl}
 			type='text'
 			placeholder='Search movies...'
 			value={query}
